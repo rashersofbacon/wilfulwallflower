a0e46172440a7a47b2535d04d5827cdc40aa7b85
David Bacon <new_davros80@hotmail.com>
2012-10-06 14:41:56 +1000

    Created application using Refinery CMS 2.0.8

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wilfulwallflower::Application.config.secret_token = '61539c9187bad758ef380d7aec9b985500a07af680b138761f719fdf21ba762c9eaa5373cb7c5ff25e339ad6edf7d47e443d58f250a7e49fde5319796b63e62b'
